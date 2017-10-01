9e479469897db8eb3c871ea38822603af8849f78
pomber <pombopombopombo@gmail.com>
2017-10-01 15:13:21 -0300

    Add table

--- src/demo.js
+++ src/demo.js
@@ -1,9 +1,39 @@
 /** @jsx Reactish.createElement */
 
 export function using(Reactish) {
-  class Demo extends Reactish.Component {
+  class Cell extends Reactish.Component {
     render() {
-      return <div>Hello</div>;
+      return <td>{this.props.text}</td>;
+    }
+  }
+
+  class Demo extends Reactish.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        elapsed: 0,
+        size: 4,
+        period: 1000
+      };
+      this.tick = this.tick.bind(this);
+      this.tick();
+    }
+    tick() {
+      setTimeout(() => {
+        this.setState(s => ({ elapsed: s.elapsed + 1 }));
+        this.tick();
+      }, this.state.period);
+    }
+    render() {
+      const { elapsed, size } = this.state;
+      const array = Array(size).fill();
+      const row = array.map((x, i) => <Cell text={elapsed % 10} key={i} />);
+      const rows = array.map((x, i) => <tr key={i}>{row}</tr>);
+      return (
+        <table>
+          <tbody>{rows}</tbody>
+        </table>
+      );
     }
   }
 
