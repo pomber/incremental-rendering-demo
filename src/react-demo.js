a016c55bff7db1d7676b780ac4af33d2b59c2260
pomber <pombopombopombo@gmail.com>
2017-10-01 18:52:57 -0300

    More layouting

--- src/react-demo.js
+++ src/react-demo.js
@@ -9,4 +9,14 @@ const Demo = using({
   render: ReactDOM.render
 });
 
-ReactDOM.render(<Demo />, document.getElementById("root"));
+ReactDOM.render(
+  [
+    <h3 key="1">This section is updated using React</h3>,
+    <Demo key="2" />,
+    <footer key="3">
+      Now try it using <a href="react-fiber.html">React async rendering</a> or{" "}
+      <a href="react-fiber.html">Didact Fiber</a>
+    </footer>
+  ],
+  document.getElementById("root")
+);
