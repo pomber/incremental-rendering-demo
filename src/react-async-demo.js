d338b31e398fb5d8851ff9c8404540183535998a
pomber <pombopombopombo@gmail.com>
2017-10-01 22:04:34 -0300

    Remove commas

--- src/react-async-demo.js
+++ src/react-async-demo.js
@@ -12,8 +12,8 @@ const Demo = using({
 
 ReactDOM.render(
   <AsyncComponent>
-    <h3 key="1">This section is updated using React (async)</h3>,
-    <Demo key="2" />,
+    <h3 key="1">This section is updated using React (async)</h3>
+    <Demo key="2" />
     <footer key="3">
       Now try it using <a href="react-sync.html">React sync rendering</a> or{" "}
       <a href="didact.html">Didact Fiber</a>
