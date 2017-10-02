b40c8c9970ca9abb6284d68350835a95a05de14a
pomber <pombopombopombo@gmail.com>
2017-10-01 22:02:35 -0300

    Add async rendering

--- src/react-async-demo.js
+++ src/react-async-demo.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { using } from "./demo";
 import "./bleeding-thread";
 
+const AsyncComponent = React.unstable_AsyncComponent;
 const Demo = using({
   createElement: React.createElement,
   Component: React.Component,
@@ -10,13 +11,13 @@ const Demo = using({
 });
 
 ReactDOM.render(
-  [
+  <AsyncComponent>
     <h3 key="1">This section is updated using React (async)</h3>,
     <Demo key="2" />,
     <footer key="3">
       Now try it using <a href="react-sync.html">React sync rendering</a> or{" "}
       <a href="didact.html">Didact Fiber</a>
     </footer>
-  ],
+  </AsyncComponent>,
   document.getElementById("root")
 );
